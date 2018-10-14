d6cdea1d8f6b8cec6903087714ad78e4585e0182
Frauchi <sterluc@gmail.com>
2018-10-14 14:19:29 +0200

    Adds English Tic-Tac-Toe

--- Tic-Tac-Toe.py
+++ Tic-Tac-Toe.py
@@ -1,130 +1,130 @@
-tablero = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
-jugador = 1
+board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+player = 1
 
 
-def traducir_ficha(valor):
-	if valor == 0:
-		ficha = " "
-	elif valor == 1:
-		ficha = "X"
+def translate_tile(value):
+	if value == 0:
+		tile = " "
+	elif value == 1:
+		tile = "X"
 	else:
-		ficha = "O"
+		tile = "O"
 
-	return ficha
+	return tile
 
 
-def pintar_tablero(tablero_actual):
+def print_board(current_board):
 	print("")
 	print("  A   B   C")
-	print("1", traducir_ficha(tablero_actual[0][0]), "|", traducir_ficha(tablero_actual[0][1]), "|", traducir_ficha(tablero_actual[0][2]))
+	print("1", translate_tile(current_board[0][0]), "|", translate_tile(current_board[0][1]), "|", translate_tile(current_board[0][2]))
 	print("  ---------")
-	print("2", traducir_ficha(tablero_actual[1][0]), "|", traducir_ficha(tablero_actual[1][1]), "|", traducir_ficha(tablero_actual[1][2]))
+	print("2", translate_tile(current_board[1][0]), "|", translate_tile(current_board[1][1]), "|", translate_tile(current_board[1][2]))
 	print("  ---------")
-	print("3", traducir_ficha(tablero_actual[2][0]), "|", traducir_ficha(tablero_actual[2][1]), "|", traducir_ficha(tablero_actual[2][2]))
+	print("3", translate_tile(current_board[2][0]), "|", translate_tile(current_board[2][1]), "|", translate_tile(current_board[2][2]))
 	print("")
 
 
-def traducir_columna(letra_introducida):
-	letra_introducida = letra_introducida.upper()
+def translate_column(letter):
+	letter = letter.upper()
 
-	if letra_introducida == "A":
-		columna_traducida = 0
-	elif letra_introducida == "B":
-		columna_traducida = 1
+	if letter == "A":
+		translated_column = 0
+	elif letter == "B":
+		translated_column = 1
 	else:
-		columna_traducida = 2
+		translated_column = 2
 
-	return columna_traducida
-
-
-def traducir_fila(digito_introducido):
-	fila_traducida = int(digito_introducido) - 1
-
-	return fila_traducida
+	return translated_column
 
 
-def comprobar_raya(raya_introducida, tablero_actualizado):
-	celda1 = raya_introducida[0]
-	fila1 = celda1[0]
-	columna1 = celda1[1]
-	valor1 = tablero_actualizado[fila1][columna1]
+def translate_row(digit):
+	translated_row = int(digit) - 1
+
+	return translated_row
+
+
+def check_line(line, updated_board):
+	cell1 = line[0]
+	row1 = cell1[0]
+	column1 = cell1[1]
+	value1 = updated_board[row1][column1]
 	
-	celda2 = raya_introducida[1]
-	fila2 = celda2[0]
-	columna2 = celda2[1]
-	valor2 = tablero_actualizado[fila2][columna2]
+	cell2 = line[1]
+	row2 = cell2[0]
+	column2 = cell2[1]
+	value2 = updated_board[row2][column2]
 	
-	celda3 = raya_introducida[2]
-	fila3 = celda3[0]
-	columna3 = celda3[1]
-	valor3 = tablero_actualizado[fila3][columna3]
+	cell3 = line[2]
+	row3 = cell3[0]
+	column3 = cell3[1]
+	value3 = updated_board[row3][column3]
 	
-	if valor1 == valor2 == valor3:
-		return valor1
+	if value1 == value2 == value3:
+		return value1
 	else:
 		return 0
 
 
-def obtener_ganador(tablero_actualizado):
-	fila_1 = [(0,0), (0,1), (0,2)]
-	fila_2 = [(1,0), (1,1), (1,2)]
-	fila_3 = [(2,0), (2,1), (2,2)]
-	columna_1 = [(0,0), (1,0), (2,0)]
-	columna_2 = [(0,1), (1,1), (2,1)]
-	columna_3 = [(0,2), (1,2), (2,2)]
+def obtain_winner(updated_board):
+	row1 = [(0,0), (0,1), (0,2)]
+	row2 = [(1,0), (1,1), (1,2)]
+	row3 = [(2,0), (2,1), (2,2)]
+	column1 = [(0,0), (1,0), (2,0)]
+	column2 = [(0,1), (1,1), (2,1)]
+	column3 = [(0,2), (1,2), (2,2)]
 	diagonal = [(0,0), (1,1), (2,2)]
 	antidiagonal = [(0,2), (1,1), (2,0)]
 
-	rayas = [fila_1, fila_2, fila_3, columna_1, columna_2, columna_3, diagonal, antidiagonal]
+	lines = [row1, row2, row3, column1, column2, column3, diagonal, antidiagonal]
 	
-	for raya in rayas:
-		valor_raya = comprobar_raya(raya, tablero_actualizado)
-		if valor_raya != 0:
-			return valor_raya
+	for line in lines:
+		line_value = check_line(line, updated_board)
+		if line_value != 0:
+			return line_value
 	
 	return 0
 
 
-def tablero_lleno(tablero):
-	for fila in tablero:
-		for valor in fila:
-			if valor == 0:
+def board_is_full(board):
+	for row in board:
+		for value in row:
+			if value == 0:
 				return False
 	return True	
 
 
-nombre1 = input("Introduce el nombre del jugador 1: ")
-nombre2 = input("Introduce el nombre del jugador 2: ")
+name1 = input("Write player 1's name: ")
+name2 = input("Write player 2's name: ")
 
 
-while obtener_ganador(tablero) == 0 and tablero_lleno(tablero) != True:
-	pintar_tablero(tablero)
-	movimiento = input("Coloca tu ficha: ")
-	letra = movimiento[0]
-	digito = movimiento[1]
+while obtain_winner(board) == 0 and board_is_full(board) != True:
+	print_board(board)
+	move = input("Place your tile: ")
+	letter = move[0]
+	digit = move[1]
 
-	columna = traducir_columna(letra)
-	fila = traducir_fila(digito)
+	column = translate_column(letter)
+	row = translate_row(digit)
 
-	if tablero[fila][columna] != 0:
-		print("La casilla seleccionada ya está llena, por favor, elige otra.")
+	if board[row][column] != 0:
+		print("The cell you picked is already taken, please, choose another.")
 	else:
-		tablero[fila][columna] = jugador #actualiza
+		board[row][column] = player #actualiza
 	
-		if jugador == 1:
-			jugador = 2
-		elif jugador == 2:
-			jugador = 1
+		if player == 1:
+			player = 2
+		elif player == 2:
+			player = 1
 
 
-ganador = obtener_ganador(tablero)
+winner = obtain_winner(board)
 
-if ganador == 0:
-	print("Empate")
+if winner == 0:
+	print("Draw")
 else:
-	if ganador == 1:
-		nombre = nombre1
+	if winner == 1:
+		name = name1
 	else:
-		nombre = nombre2
+		name = name2
 
-	print("Enhorabuena, " + nombre + "!")
+	print("Condragulations, " + name + "!")
